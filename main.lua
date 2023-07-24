42c5ea7c0fb95a0a133dd5df6866e49a0c1482f0
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-24 17:23:08 -0300

    adds wirtual windows to main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,30 @@
-local WINDOW_WIDTH = 1280
-local WINDOW_HEIGHT = 720
+local push = require("push")
+local constants = require("constants")
 
 function love.load()
-	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-		fullscreen = false,
-		resizable = false,
-		vsync = true,
-	})
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	push:setupScreen(
+		constants.VIRTUAL_WIDTH,
+		constants.VIRTUAL_HEIGHT,
+		constants.WINDOW_WIDTH,
+		constants.WINDOW_HEIGHT,
+		{
+			fullscreen = false,
+			resizable = false,
+			vsync = true,
+		}
+	)
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		love.event.quit()
+	end
+end
+
+function love.draw()
+	push:apply("start")
+	love.graphics.printf("Hello pong", 0, constants.VIRTUAL_HEIGHT / 2 - 6, constants.VIRTUAL_WIDTH, "center")
+	push:apply("end")
 end
