425c0658b2309edf876c7c24ae6d8a29868d826f
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-28 19:20:04 -0300

    feat: organizes better constants and variables file

---
+++ globals/constants.lua
@@ -0,0 +1,14 @@
+local constants = {
+
+	WINDOW_WIDTH = 1280,
+	WINDOW_HEIGHT = 720,
+
+	VIRTUAL_WIDTH = 432,
+	VIRTUAL_HEIGHT = 243,
+
+	PADDLE_SPEED = 200,
+
+	SMALL_FONT = love.graphics.newFont("font.ttf", 8),
+}
+
+return constants
---
+++ globals/variables.lua
@@ -0,0 +1,7 @@
+local variables = {
+	gameState = "start",
+	playerAScore = 0,
+	playerBScore = 0,
+}
+
+return variables
