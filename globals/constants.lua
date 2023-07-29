ae4d9983d23fb9356bf05ac579388d0bb2d180db
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 01:24:02 -0300

    refact: updates constants

--- globals/constants.lua
+++ globals/constants.lua
@@ -11,4 +11,10 @@ constants.PADDLE_SPEED = 200
 constants.SMALL_FONT = love.graphics.newFont("font.ttf", 8)
 constants.SCORE_FONT = love.graphics.newFont("font.ttf", 32)
 
+constants.sounds = {}
+
+constants.sounds.hit = love.audio.newSource("sound/hit.wav", "static")
+constants.sounds.score = love.audio.newSource("sound/score.wav", "static")
+constants.sounds.wall_hit = love.audio.newSource("sound/wall_hit.wav", "static")
+
 return constants
