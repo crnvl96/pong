342cb92cf04985702607a9943f48fe28a81cbb4b
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 01:24:07 -0300

    refact: updates variables

--- globals/variables.lua
+++ globals/variables.lua
@@ -3,5 +3,7 @@ local variables = {}
 variables.gameState = "start"
 variables.playerAScore = 0
 variables.playerBScore = 0
+variables.servingPlayer = 1
+variables.winningPlayer = 0
 
 return variables
