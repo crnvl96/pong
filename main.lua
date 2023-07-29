101ceb62c5dddb273407f2518e86183535cd93ac
Ádran Carnavale <adran.carnavale@gmail.com>
2023-07-29 01:24:19 -0300

    updates on main function

--- main.lua
+++ main.lua
@@ -14,6 +14,24 @@ local function displayFPS()
 	love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
 end
 
+function love.resize(width, height)
+	push:resize(width, height)
+end
+
+local function displayScore()
+	love.graphics.setFont(constants.SCORE_FONT)
+	love.graphics.print(
+		tostring(variables.playerAScore),
+		constants.VIRTUAL_WIDTH / 2 - 50,
+		constants.VIRTUAL_HEIGHT / 3
+	)
+	love.graphics.print(
+		tostring(variables.playerBScore),
+		constants.VIRTUAL_WIDTH / 2 + 30,
+		constants.VIRTUAL_HEIGHT / 3
+	)
+end
+
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
 	love.window.setTitle("Pong")
@@ -27,7 +45,7 @@ function love.load()
 		constants.WINDOW_HEIGHT,
 		{
 			fullscreen = false,
-			resizable = false,
+			resizable = true,
 			vsync = true,
 		}
 	)
@@ -45,15 +63,100 @@ function love.keypressed(key)
 		love.event.quit()
 	elseif key == "enter" or key == "return" then
 		if variables.gameState == "start" then
+			variables.gameState = "serve"
+		elseif variables.gameState == "serve" then
 			variables.gameState = "play"
-		else
-			variables.gameState = "start"
+		elseif variables.gameState == "done" then
+			variables.gameState = "serve"
 			ball:reset()
+			variables.playerAScore = 0
+			variables.playerBScore = 0
+
+			if variables.winningPlayer == 1 then
+				variables.servingPlayer = 2
+			else
+				variables.servingPlayer = 1
+			end
 		end
 	end
 end
 
 function love.update(dt)
+	if variables.gameState == "serve" then
+		ball.dy = math.random(-50, 50)
+		if variables.servingPlayer == 1 then
+			ball.dx = math.random(140, 200)
+		else
+			ball.dx = -math.random(140, 200)
+		end
+	elseif variables.gameState == "play" then
+		if ball:collides(playerA) then
+			constants.sounds.hit:play()
+
+			ball.dx = -ball.dx * 1.03
+			ball.x = playerA.x + 5
+
+			if ball.dy < 0 then
+				ball.dy = -math.random(10, 150)
+			else
+				ball.dy = math.random(10, 150)
+			end
+		end
+
+		if ball:collides(playerB) then
+			constants.sounds.hit:play()
+
+			ball.dx = -ball.dx * 1.03
+			ball.x = playerB.x - 4
+
+			if ball.dy < 0 then
+				ball.dy = -math.random(10, 150)
+			else
+				ball.dy = math.random(10, 150)
+			end
+		end
+
+		if ball.y <= 0 then
+			ball.y = 0
+			ball.dy = -ball.dy
+			constants.sounds.wall_hit:play()
+		end
+
+		if ball.y >= constants.VIRTUAL_HEIGHT - 4 then
+			ball.y = constants.VIRTUAL_HEIGHT - 4
+			ball.dy = -ball.dy
+			constants.sounds.wall_hit:play()
+		end
+
+		if ball.x < 0 then
+			constants.sounds.score:play()
+			variables.servingPlayer = 1
+			variables.playerBScore = variables.playerBScore + 1
+
+			if variables.playerBScore == 10 then
+				variables.winningPlayer = 2
+				variables.gameState = "done"
+			else
+				ball:reset()
+				variables.gameState = "serve"
+			end
+		end
+
+		if ball.x > constants.VIRTUAL_WIDTH then
+			constants.sounds.score:play()
+			variables.servingPlayer = 2
+			variables.playerAScore = variables.playerAScore + 1
+
+			if variables.playerAScore == 10 then
+				variables.winningPlayer = 1
+				variables.gameState = "done"
+			else
+				ball:reset()
+				variables.gameState = "serve"
+			end
+		end
+	end
+
 	if love.keyboard.isDown("w") then
 		playerA.dy = -constants.PADDLE_SPEED
 	elseif love.keyboard.isDown("s") then
@@ -71,38 +174,6 @@ function love.update(dt)
 	end
 
 	if variables.gameState == "play" then
-		if ball:collides(playerA) then
-			ball.dx = -ball.dx * 1.03
-			ball.x = playerA.x + 5
-
-			if ball.dy < 0 then
-				ball.dy = -math.random(10, 150)
-			else
-				ball.dy = math.random(10, 150)
-			end
-		end
-
-		if ball:collides(playerB) then
-			ball.dx = -ball.dx * 1.03
-			ball.x = playerB.x - 4
-
-			if ball.dy < 0 then
-				ball.dy = -math.random(10, 150)
-			else
-				ball.dy = math.random(10, 150)
-			end
-		end
-
-		if ball.y <= 0 then
-			ball.y = 0
-			ball.dy = -ball.dy
-		end
-
-		if ball.y >= constants.VIRTUAL_HEIGHT - 4 then
-			ball.y = constants.VIRTUAL_HEIGHT - 4
-			ball.dy = -ball.dy
-		end
-
 		ball:update(dt)
 	end
 
@@ -117,23 +188,36 @@ function love.draw()
 
 	love.graphics.setFont(constants.SMALL_FONT)
 
-	if variables.gameState == "start" then
-		love.graphics.printf("Welcome to Pong!", 0, 20, constants.VIRTUAL_WIDTH, "center")
-	else
-		love.graphics.printf("Play ball!", 0, 20, constants.VIRTUAL_WIDTH, "center")
-	end
+	displayScore()
 
-	love.graphics.setFont(constants.SCORE_FONT)
-	love.graphics.print(
-		tostring(variables.playerAScore),
-		constants.VIRTUAL_WIDTH / 2 - 50,
-		constants.VIRTUAL_HEIGHT / 3
-	)
-	love.graphics.print(
-		tostring(variables.playerBScore),
-		constants.VIRTUAL_WIDTH / 2 + 30,
-		constants.VIRTUAL_HEIGHT / 3
-	)
+	if variables.gameState == "start" then
+		love.graphics.setFont(constants.SMALL_FONT)
+		love.graphics.printf("Welcome to Pong!", 0, 10, constants.VIRTUAL_WIDTH, "center")
+		love.graphics.printf("Press Enter to begin!", 0, 20, constants.VIRTUAL_WIDTH, "center")
+	elseif variables.gameState == "serve" then
+		love.graphics.setFont(constants.SMALL_FONT)
+		love.graphics.printf(
+			"Player " .. tostring(variables.servingPlayer) .. "'s serve!",
+			0,
+			10,
+			constants.VIRTUAL_WIDTH,
+			"center"
+		)
+		love.graphics.printf("Press Enter to serve!", 0, 20, constants.VIRTUAL_WIDTH, "center")
+	elseif variables.gameState == "play" then
+		-- no messages while playing
+	elseif variables.gameState == "done" then
+		love.graphics.setFont(constants.SCORE_FONT)
+		love.graphics.printf(
+			"Player " .. tostring(variables.winningPlayer) .. " wins!",
+			0,
+			10,
+			constants.VIRTUAL_WIDTH,
+			"center"
+		)
+		love.graphics.setFont(constants.SMALL_FONT)
+		love.graphics.printf("Press Enter to restart!", 0, 60, constants.VIRTUAL_WIDTH, "center")
+	end
 
 	playerA:render()
 	playerB:render()
